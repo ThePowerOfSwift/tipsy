c37d2d73924221848530f86fbcd56bb1f2c53ccf
Lia Zadoyan <liazadoyan@gmail.com>
2017-07-31 22:46:01 -0700

    persist settings to remember default tip

--- tipsy/TipViewController.swift
+++ tipsy/TipViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    private var bill = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -32,14 +35,29 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calculateTip(_ sender: AnyObject) {
+        bill = Double(billField.text!) ?? 0
+        updateTip()
+    }
+    
+    private func updateTip() {
         let tipPercentages = [0.18, 0.2, 0.25]
         
-        let bill = Double(billField.text!) ?? 0
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let defaults = UserDefaults.standard
+        let intValue = defaults.object(forKey: "default_tip_index")
+        if (intValue != nil) {
+            tipControl.selectedSegmentIndex = intValue as! Int
+            updateTip()
+        }
     }
 }
 
