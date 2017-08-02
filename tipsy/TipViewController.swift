1ea6d4f34bf421c8b8306555aded22f3d2329c49
Christopher de Haan <chrisdhaan@gmail.com>
2017-08-01 21:21:22 -0700

    Code Review
    - Updated code with some best practices

--- tipsy/TipViewController.swift
+++ tipsy/TipViewController.swift
@@ -10,34 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet private weak var tipLabel: UILabel!
     
-    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet private weak var totalLabel: UILabel!
     
-    @IBOutlet weak var billField: UITextField!
-    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet private weak var billField: UITextField!
+    @IBOutlet private weak var tipControl: UISegmentedControl!
     
     private var bill = 0.0
+    private let defaults = UserDefaults.standard
+    
+    // MARK: - Lifecycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Get the last time the view closed
+        let timeAppClosed = defaults.object(forKey: UserDefaultKeys.timeAppClosed) as? Int ?? 0
+        // Set the default tip option if one exists, else default to 18%
+        if let defaultTipIndex = defaults.object(forKey: UserDefaultKeys.defaultTipIndex) as? Int {
+            tipControl.selectedSegmentIndex = defaultTipIndex
+            updateTip()
+        }
+        // Get the current time
+        let currentTime = Int(NSDate().timeIntervalSince1970)
+        // Check if enough time has passed to update the view
+        if currentTime - timeAppClosed < 600,
+            let billLastClosed = defaults.object(forKey: UserDefaultKeys.billLastClosed) as? String,
+            let percentageLastClosed = defaults.object(forKey: UserDefaultKeys.percentageLastClosed) as? Int {
+            // Set bill
+            billField.text = billLastClosed
+            bill = Double(billLastClosed) ?? 0
+            // Set tip percentage
+            tipControl.selectedSegmentIndex = percentageLastClosed
+            // Update view
+            updateTip()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        defaults.set(Int(NSDate().timeIntervalSince1970), forKey: UserDefaultKeys.timeAppClosed)
+        defaults.set(tipControl.selectedSegmentIndex, forKey: UserDefaultKeys.percentageLastClosed)
+        defaults.set(billField.text, forKey: UserDefaultKeys.billLastClosed)
+        
+        defaults.synchronize()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: - Button Methods
+    
     @IBAction func onTap(_ sender: AnyObject) {
         view.endEditing(true)
     }
     
     @IBAction func calculateTip(_ sender: AnyObject) {
-        bill = Double(billField.text!) ?? 0
+        if let billFieldText = billField.text,
+            let billFieldValue = Double(billFieldText) {
+            bill = billFieldValue
+        }
         updateTip()
     }
+    
+    // MARK: - Private Methods
     
     private func updateTip() {
         let tipPercentages = [0.18, 0.2, 0.25]
@@ -48,39 +92,4 @@ class ViewController: UIViewController {
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        let defaults = UserDefaults.standard
-        let intValue = defaults.object(forKey: "default_tip_index")
-        var timeAppClosed = defaults.object(forKey: "time_app_closed") as? Int
-        if (timeAppClosed == nil) {
-            timeAppClosed = 0
-        }
-        let currentTime = Int(NSDate().timeIntervalSince1970)
-        if (intValue != nil) {
-            tipControl.selectedSegmentIndex = intValue as! Int
-            updateTip()
-        }
-        if (currentTime - timeAppClosed! < 600) {
-            billField.text = defaults.object(forKey: "bill_last_closed") as? String
-            bill = Double(billField.text!) ?? 0
-            tipControl.selectedSegmentIndex = defaults.object(forKey: "percentage_last_closed") as! Int
-            updateTip()
-        }
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        let defaults = UserDefaults.standard
-        
-        defaults.set(Int(NSDate().timeIntervalSince1970), forKey: "time_app_closed")
-        defaults.set(tipControl.selectedSegmentIndex, forKey: "percentage_last_closed")
-        defaults.set(billField.text, forKey: "bill_last_closed")
-        
-        defaults.synchronize()
-    }
 }
-
